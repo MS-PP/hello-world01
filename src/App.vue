10ae4e2c2a0339736c488f72d10a9d2dc9113730
zhangsan <2345654@qq.com>
2021-03-03 00:17:39 +0800

    第一次上 传到github上

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <p>java0821 全体同学 越来越帅</p>
+      <p>java0821 彭蕤祥  高薪就业</p>
+      <p>java0821 全体同学 6666666</p>
+      <p>java0821 全体同学 6666666</p>
+      <p>java0821 全体同学 6666666</p>
+   <img src="https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3512738235,3373857725&fm=26&gp=0.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
